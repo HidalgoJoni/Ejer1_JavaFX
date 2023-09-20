87fa6c1549a0aa1222426890e1cf23e57121ac72
HidalgoJoni <jonigarciah@gmail.com>
2023-09-20 12:32:38 +0200

    1ra prueba obi wan Creada clase de pruebas para apartado obi wan en main

---
+++ src/application/prueba_obiwan.java
@@ -0,0 +1,56 @@
+package application;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class prueba_obiwan extends Application {
+	public static void main(String[] args) {
+		
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) {
+		//Mensaje de texto en ventana
+		Label nombre = new Label("Introduce tu nombre");
+		TextField nombreIntroducido = new TextField();
+        Label msg = new Label();
+
+        //crea los botones de la ventana
+		Button saludar = new Button("Hola!");
+        Button cerrar = new Button("Salir");
+
+		//cerrar ventana
+        cerrar.setOnAction(e -> Platform.exit());
+        
+        // Configura la accion del boton saludar
+        saludar.setOnAction(e -> {
+            String name = nombreIntroducido.getText();
+			String nombreSaludo = "obi wan";
+            if (name.toLowerCase() == nombreSaludo.toLowerCase()) {
+                msg.setText("¡General Kenobi!");
+            } else {
+                msg.setText("Hola " + name);
+            }
+        });
+
+        // Configura la accion del boton cerrar
+        cerrar.setOnAction(e -> Platform.exit());
+
+        // Crea organizacion VBox
+        VBox raiz = new VBox();
+
+        // Añade todo al VBox
+        raiz.getChildren().addAll(nombre, nombreIntroducido, msg, saludar, cerrar);
+        Scene escena = new Scene(raiz, 350, 150);
+        stage.setScene(escena);
+        stage.setTitle("Ejercicio Obi Wan");
+        stage.setResizable(false);
+        stage.show();
+    }
+};
